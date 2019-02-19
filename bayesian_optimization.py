4b934241bf57adc867c2c1c3b2a803e61568d596
Kareem Jeiroudi <kareemjeiroudi@gmail.com>
2019-02-19 18:56:58 +0100

    Loaded the data into Bayesian improved model

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -5,11 +5,10 @@ import numpy as np
 import math
 
 from keras import backend as K
-from keras.models import Sequential
-from keras.layers import InputLayer, Input, Dense
+from keras.models import Sequential, load_model
+from keras.layers import InputLayer, Input, Dense, Dropout
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
-from keras.models import load_model
 
 ## pip install h5py scikit-optimize
 ## once you have that installed, you need to install a bunch of thing sfrom scikit optimize.
@@ -21,37 +20,47 @@ from skopt.plots import plot_objective, plot_evaluations
 from skopt.plots import plot_histogram, plot_objective_2D
 from skopt.utils import use_named_args
 
+from sklearn.metrics import roc_auc_score ## Computer Area Under the Curve
+
+from datetime import datetime ## time the Optimization time
 ## Before starting the session
 print(tf.__version__)
 print(tf.keras.__version__)
 print(skopt.__version__)
-## to set up this search space, I first need to define the search space dimension, what parameters are we gonna explore.
-## for each of the parameters, we define a dimension explicitly
 
-## the learning rate is any real number between 0.000001 and 0.1. But the seraching is done not in bounds.
+## Load Datset
+train_samples = np.loadtxt("data/train_samples.txt", delimiter=' ', comments='# ', encoding=None)
+train_labels = np.loadtxt("data/train_labels.txt", delimiter=' ', comments='# ', encoding=None)
+valid_samples = np.loadtxt("data/valid_samples.txt", delimiter=' ', comments='# ', encoding=None)
+valid_labels = np.loadtxt("data/valid_labels.txt", delimiter=' ', comments='# ', encoding=None)
+
+## To set up this search space, I first need to define the search space dimension, what parameters are we gonna explore.
+## for each of the parameters, we define a dimension explicitly
+##
+## The learning rate is any real number between 0.000001 and 0.1. But the seraching is done not in bounds.
 ## 'log-uniform' specifies how the trasformation(updates) of these values is 
 learning_rate_dim = Real(low=1e-6, high=1e-2, prior='log-uniform', name='learning_rate')
 ## The number of alyers on the other hand is explored in bounds, increments are done using integers
 dense_layers_dim = Integer(low=1, high=5, name='dense_layers')
 ## We'll also different number of nodes in a layer
 nodes_dim = Integer(low=5, high=512, name='nodes')
-## finally we have a Categorical dimension, this needs to be specified explicitly, because scikit-learn
+## Finally we have a Categorical dimension, this needs to be specified explicitly, because scikit-learn
 ## isn't gonna generate some randomly for you
 activation_dim = Categorical(categories=['relu', 'sigmoid'], name='activation')
-## combine all the parameters into a list, so that we can pass it to a function
+## Combine all the parameters into a list, so that we can pass it to a function
 dimensions = [learning_rate_dim,
 			dense_layers_dim,
 			nodes_dim,
 			activation_dim]
 
 
-## to kick off, it's helpful to start the serach using a set of hyperparameters that we
+## To kick off, it's helpful to start the serach using a set of hyperparameters that we
 ## intuitively know performes well
-## these default parameters aren't horrible, but they don't perform great either
+## These default parameters aren't horrible, but they don't perform great either
 default_parameters = [1e-5, 1, 16, 'relu']
 
 
-
+## To log the performance of the model
 def log_dir_name(learning_rate, dense_layers, nodes, activation):
 	"""
 	Creates a directory named after the set of hyperparameters that was recently selected. A helper function
@@ -65,7 +74,7 @@ def log_dir_name(learning_rate, dense_layers, nodes, activation):
 
 
 ## This funcion is copied from my previous solution on Grid SearchCV
-def create_model(learning_rate, dense_layers, nodes, activation):
+def create_model(learning_rate, dense_layers, nodes, activation, dropout_rate=0.1):
 	"""
 	A helper function for the classifier to help construct a model after each run.
 
@@ -78,7 +87,7 @@ def create_model(learning_rate, dense_layers, nodes, activation):
 	"""
 	model = Sequential()
 	## Note that the input-shape must be a tupe containing the data-size.
-	model.add(InputLayer(input_shape=(here,)))
+	model.add(InputLayer(input_shape=train_samples.input_shape))
 	## Needful only in case of convolutional layers.
 	# model.add(Reshape(img_shape_full))
 	for i in range(dense_layers):
@@ -86,14 +95,15 @@ def create_model(learning_rate, dense_layers, nodes, activation):
 		name = 'layer_dense_{0}.format(i+1)'
 		## Add these fully-connected layers to the model.
 		model.add(Dense(nodes, activation=activation, name=name))
+		model.add(Dropout(dropout_rate))
 
 	## Last output layer with softmax-activation.
 	## Used heavily for classification.
-	model.add(Dense(num_classes, activation='softmax'))
+	model.add(Dense(1, activation='sigmoid'))
 
 	optimizer = Adam(lr=learning_rate)
 	## Compile the model
-	model.Compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+	model.Compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 	return model
 
@@ -116,36 +126,37 @@ def fitness(learning_rate, dense_layers, nodes, activation):
 	## Create the neural network with these hyperparameters.
 	model = create_model(learning_rate, dense_layers, nodes, activation)
 	## Create log files for the model.
-	callback_log = TensorBoard(
-		log_dir=log_dir,
-		histogram_freq=0,
-		batch_size=32,
-		write_graph=True,
-		write_grads=False,
-		write_images=False)
+	## Not important for now!
+	# callback_log = TensorBoard(
+	# 	log_dir=log_dir,
+	# 	histogram_freq=0,
+	# 	batch_size=32,
+	# 	write_graph=True,
+	# 	write_grads=False,
+	# 	write_images=False)
 	## Use Keras to train the model.
 	history = model.fit(x=train_data,
 		y=train_labels,
 		epochs=3,
-		batch_size=128,
-		validation_data=validation_data,
-		callbacks=[callback_log])
+		batch_size=128)
+		#callbacks=[callback_log])
 	## Get the classification accuracy on the validation set after the last training epoch.
-	accuracy = history.history['val_acc'][-1]
-
+	# accuracy = history.history['val_acc'][-1]
+	predictions = model.predict(valid_samples)
+	auc = roc_auc_score(valid_labels, predictions)
 	## Print the calssification accuracy.
 	print()
-	print("Accuracy: {0:.2%}".format(accuracy))
+	print("AUC = : {0:.2%}".format(auc))
 	print()
 
 	## Save the model if it improves on the best-found performance.
 	## We use the global keyword so we update the variable outside of this function.
-	global best_accuracy
-	if accuracy > best_accuracy:
+	global best_auc
+	if auc > best_auc:
 		## Save the new model to harddisk.
 		model.save(path_best_model)
 		## Update the classification accuracy.
-		best_accuracy = accuracy
+		best_auc = auc
 
 	## Delete the Keras model with these heyper parameters from memory.
 	## Also clear the session.
@@ -165,12 +176,12 @@ serach_result = gp_minimize(func=fitness,
 	x0=default_parameters)
 
 ## Plot the progress of the optimizer.
-plot_convergence(serach_result)
-print("Best serach results:")
-print(serach_result.x)
-space = serach_result.space
-print(space.point_to_dict(serach_result.x))
-print("Lowest fitness value:")
-print(search_result.fun)
-print(sorted(zip(search_result.func_vals, search_result.x_iters)))
+# plot_convergence(serach_result)
+# print("Best serach results:")
+# print(serach_result.x)
+# space = serach_result.space
+# print(space.point_to_dict(serach_result.x))
+# print("Lowest fitness value:")
+# print(search_result.fun)
+# print(sorted(zip(search_result.func_vals, search_result.x_iters)))
 
