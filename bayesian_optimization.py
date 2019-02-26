588592b5d2a98e57c49f58d525820216feee3293
Kareem Jeiroudi <Kareemjeiroudi@gmail.com>
2019-02-26 10:44:52 +0100

    Created csv file to assess the bayesian optimization

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -1,32 +1,36 @@
-%matplotlib inline
-import matplotlib.pyplot as plt
+# %matplotlib inline
 import tensorflow as tf
+#import tensorflow.keras as K
 import numpy as np
 import math
+import matplotlib
+matplotlib.use('GTKAgg')
+import matplotlib.pyplot as plt
 
-from keras import backend as K
+# from keras import backend as K
 from keras.models import Sequential, load_model
+# from K.models import Sequential, load_model
 from keras.layers import InputLayer, Input, Dense, Dropout
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
-
+from keras.backend import clear_session
 ## pip install h5py scikit-optimize
-## once you have that installed, you need to install a bunch of thing sfrom scikit optimize.
+## once you have that installed, you can run the following code.
 import skopt
 from skopt import gp_minimize, forest_minimize
 from skopt.space import Real, Categorical, Integer
+matplotlib.use('GTKAgg')
 from skopt.plots import plot_convergence
+matplotlib.use('GTKAgg')
 from skopt.plots import plot_objective, plot_evaluations
-from skopt.plots import plot_histogram, plot_objective_2D
+matplotlib.use('GTKAgg')
+import csv
+from timeit import default_timer as timer
+
+#from skopt.plots import plot_histogram, plot_objective_2D
 from skopt.utils import use_named_args
-
 from sklearn.metrics import roc_auc_score ## Computer Area Under the Curve
-
 from datetime import datetime ## time the Optimization time
-## Before starting the session
-print(tf.__version__)
-print(tf.keras.__version__)
-print(skopt.__version__)
 
 ## Load Datset
 train_samples = np.loadtxt("data/train_samples.txt", delimiter=' ', comments='# ', encoding=None)
@@ -65,7 +69,7 @@ def log_dir_name(learning_rate, dense_layers, nodes, activation):
 	"""
 	Creates a directory named after the set of hyperparameters that was recently selected. A helper function
 	to log the results of training every constructed model.
-	"""
+	"""	
 	# the dir-name for the TensorBoard log-dir
 	s = "./2_logs/lr_{0:.0e}_layers{1}_nodes{2}_{3}/"
 	log_dir = s.format(learning_rate, dense_layers, nodes, activation)
@@ -86,13 +90,15 @@ def create_model(learning_rate, dense_layers, nodes, activation, dropout_rate=0.
 	the output layer.
 	"""
 	model = Sequential()
-	## Note that the input-shape must be a tupe containing the data-size.
-	model.add(InputLayer(input_shape=train_samples.input_shape))
+	global train_samples
+	## Input-shape must be a tuple without the batch size.
+	input_shape = (1,) + train_samples.shape
+	model.add(InputLayer(input_shape=(len(train_samples[0]),)))
 	## Needful only in case of convolutional layers.
 	# model.add(Reshape(img_shape_full))
 	for i in range(dense_layers):
 		## Name each layer, because Keras should give them unique names.
-		name = 'layer_dense_{0}.format(i+1)'
+		name = 'layer_dense_{0}'.format(i+1)
 		## Add these fully-connected layers to the model.
 		model.add(Dense(nodes, activation=activation, name=name))
 		model.add(Dropout(dropout_rate))
@@ -103,7 +109,7 @@ def create_model(learning_rate, dense_layers, nodes, activation, dropout_rate=0.
 
 	optimizer = Adam(lr=learning_rate)
 	## Compile the model
-	model.Compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+	model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
 
 	return model
 
@@ -111,18 +117,18 @@ def create_model(learning_rate, dense_layers, nodes, activation, dropout_rate=0.
 ## Before we start training any model, let's first save the path where we'll store the best-performing model.
 best_model_path = '19_best_model.keras'
 ## A global variable to keep track of the best obtained accuracy.
-best_accuracy = 0.0
+best_auc = 0.0
 
 @use_named_args(dimensions=dimensions)
 def fitness(learning_rate, dense_layers, nodes, activation):
 	"""
 	"""
 	# Print the selected hyperparameters.
-	print('learning rate: {0:.1e}'.format(learning_rate))
+	print('learning rate: {0:.1f}'.format(learning_rate))
 	print('num_dense_layers:', dense_layers)
 	print('num_nodes:', nodes)
 	print('activation:', activation)
-	print()
+	print("")
 	## Create the neural network with these hyperparameters.
 	model = create_model(learning_rate, dense_layers, nodes, activation)
 	## Create log files for the model.
@@ -135,53 +141,64 @@ def fitness(learning_rate, dense_layers, nodes, activation):
 	# 	write_grads=False,
 	# 	write_images=False)
 	## Use Keras to train the model.
-	history = model.fit(x=train_data,
+	history = model.fit(x=train_samples,
 		y=train_labels,
-		epochs=3,
-		batch_size=128)
+		epochs=10,
+		batch_size=int(4010/4))
 		#callbacks=[callback_log])
 	## Get the classification accuracy on the validation set after the last training epoch.
 	# accuracy = history.history['val_acc'][-1]
 	predictions = model.predict(valid_samples)
 	auc = roc_auc_score(valid_labels, predictions)
 	## Print the calssification accuracy.
-	print()
+	print('')
 	print("AUC = : {0:.2%}".format(auc))
-	print()
+	print('')
 
 	## Save the model if it improves on the best-found performance.
 	## We use the global keyword so we update the variable outside of this function.
 	global best_auc
 	if auc > best_auc:
 		## Save the new model to harddisk.
-		model.save(path_best_model)
+		model.save(best_model_path)
 		## Update the classification accuracy.
 		best_auc = auc
 
 	## Delete the Keras model with these heyper parameters from memory.
 	## Also clear the session.
 	del model
-	K.clear_session()
+#    tf.keras.clear_session()
+	clear_session()
 
-	return -accuracy
+	return -auc
 
 ## Now we run our fitness function with the default hyperparameters that we set earlier.
 ## That's the reason for the @ annotation 
 fitness(x=default_parameters)
 
-serach_result = gp_minimize(func=fitness,
+search_result = gp_minimize(func=fitness,
 	dimensions=dimensions,
 	acq_func='EI', # Expected Improvement.
 	n_calls=40,
 	x0=default_parameters)
 
 ## Plot the progress of the optimizer.
-# plot_convergence(serach_result)
-# print("Best serach results:")
-# print(serach_result.x)
-# space = serach_result.space
-# print(space.point_to_dict(serach_result.x))
-# print("Lowest fitness value:")
-# print(search_result.fun)
-# print(sorted(zip(search_result.func_vals, search_result.x_iters)))
+plot_convergence(search_result)
+print("Best serach results:")
+print(search_result.x)
+print(search_result.space)
+print(point_to_dict(search_result.x))
+print("Lowest fitness value:")
+print(search_result.fun)
+zipped = sorted(zip(search_result.func_vals, search_result.x_iters))
+print()
+of = open('output_bayesian_optimization.csv', 'w')
+header="Fit Value; Learning Rate; Dense Layers; Num. Neurons; Activation\n"
+of.write(header)
+for i in zipped:
+    row = "{0}; {1}; {2}; {3}; {4};\n".format(i[0], i[1][0], i[1][1], i[1][2], i[1][3])
+    of.write(row)
 
+#of.write(str(sorted(zip(search_result.fun_vals, search_result.x_iters))))
+of.close()
+plot_objective(search_result)
