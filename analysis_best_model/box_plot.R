ceb0b4a12bd62d2f366fd7b589a1da5236945eb0
Kareem Jeiroudi <kareemjeiroudi@gmail.com>
2019-03-12 16:16:43 +0100

    Updated all plots

--- analysis_best_model/box_plot.R
+++ analysis_best_model/box_plot.R
@@ -1,57 +1,80 @@
-#### Snow Days ####
+#### Tuning Parameters ####
 
+## Reading data
 data <- read.table(pipe("pbpaste"), header=T)
-summary(snow.days)
+data2 <- read.table(pipe("pbpaste"), header=T)
 
-t.test(data$auc~data$activation, mu=0)
-levels(data$activation)
-
-## Relu vs. Selu
-boxplot(auc~activation, data=data, main="AUC in regard to Activation", 
-         xlab="Activation Function", ylab="ROC_AUC score",
-         col=(c("gold","darkgreen", "darkblue")))
-relu.auc <- data$auc[data$activation == "relu"]
-selu.auc <- data$auc[data$activation == "selu"]
-t.test(x=relu.auc, y=selu.auc, mu=0)
-
-## Adam vs. SGD
-boxplot(auc~optimizer, data=data, main="AUC in regard to Activation", 
+## Boxplot (Relu vs. Selu vs. Sigmoid)
+boxplot(auc~activation, data=data2, main="AUC with regard to Activation", 
         xlab="Activation Function", ylab="ROC_AUC score",
+        col=(c("gold","darkgreen", "darkblue")))
+
+## Wilcoxon Test (Relu vs. Selu)
+data2_no_sigmoid <- data2[data2$activation != "sigmoid",]
+wilcox.test(auc~activation, data=data2_no_sigmoid, paired=F)
+## Wilcoxon Test (SGD vs. Adam)
+wilcox.test(auc~optimizer, data=data2, paired=F)
+
+
+## Boxplot (Adam vs. SGD)
+boxplot(logauc~optimizer, data=data2, main="AUC with regard to Optimzer", 
+        xlab="Optimizer", ylab="ROC_AUC score",
         col=(c("gold","darkgreen")))
-adam.auc <- data$auc[data$optimizer == "adam"]
-SGD.auc <- data$auc[data$optimizer == "SGD"]
-t.test(x=relu.auc, y=selu.auc, mu=0)
+boxplot(log(auc)~optimizer, data=data2, main="AUC with regard to Optimzer", 
+        xlab="Optimizer", ylab="ROC_AUC score",
+        col=(c("gold","darkgreen")))
 
-length(data$activation[data$activation == "relu"])
-length(data$activation[data$activation == "selu"])
-length(data$activation[data$activation == "sigmoid"])
+## Plot the lines chart (AUC vs. Units).
+unique_units <- sort(unique(data$num_unit))
+unique_units <- sort(unique(data2$units))
+mean_auc <- matrix(nrow=4, ncol=3)
+sd_auc <- matrix(nrow=4, ncol=3)
+for (j in 1:3) {
+  mean_auc[1, j] <- mean(data2$auc[data2$units==unique_units[j] & data2$activation=="relu" & data2$optimizer=="SGD"])
+  sd_auc[1, j] <- sd(data2$auc[data2$units==unique_units[j] & data2$activation=="relu" & data2$optimizer=="SGD"])
+  mean_auc[2, j] <- mean(data2$auc[data2$units==unique_units[j] & data2$activation=="selu" & data2$optimizer=="SGD"])
+  sd_auc[2, j] <- sd(data2$auc[data2$units==unique_units[j] & data2$activation=="selu" & data2$optimizer=="SGD"])
+  mean_auc[3, j] <- mean(data2$auc[data2$units==unique_units[j] & data2$activation=="selu" & data2$optimizer=="Adam"])
+  sd_auc[3, j] <- sd(data2$auc[data2$units==unique_units[j] & data2$activation=="selu" & data2$optimizer=="Adam"])
+  mean_auc[4, j] <- mean(data2$auc[data2$units==unique_units[j] & data2$activation=="relu" & data2$optimizer=="Adam"])
+  sd_auc[4, j] <- sd(data2$auc[data2$units==unique_units[j] & data2$activation=="relu" & data2$optimizer=="Adam"])
+}
 
+plot(x=unique_units, y=mean_auc[1,], col = "red", xlab = "Number of Units", ylab = "ROC_AUC Score", 
+     ylim=c(0.5, 0.9), main = "AUC vs. Number of Units", type="o")
+polygon(x=unique_units, y=c(sd_auc[1,], -sd_auc[1,]),
+        col =  adjustcolor("red", alpha.f = 0.10), border = NA)
 
-plot(data$learning_rate, y=data$auc, type="b")
+lines(x=unique_units, y=mean_auc[2,], type = "o", col = "blue")
+lines(x=unique_units, y=mean_auc[3,], type = "o", col = "brown")
+lines(x=unique_units, y=mean_auc[4,], type = "o", col = "gold")
 
+legend(x="topright", legend=c("relu & SGD", "selu & SGD", "selu & Adam", "relu & Adam"),
+       fill = c("red", "blue", "brown", "gold"),
+       bg="lightgrey", bty="n")
 
-#### Drawing 3 lines on the same chart ####
-# Create the data for the chart.
-relu.auc <- data$auc[data$activation=="relu"]
-relu.lr <- data$learning_rate[data$activation=="relu"]
-selu.auc <- data$auc[data$activation=="selu"]
-selu.lr <- data$learning_rate[data$activation=="selu"]
-sigmoid.auc <- data$auc[data$activation=="sigmoid"]
-sigmoid.lr <- data$learning_rate[data$activation=="sigmoid"]
+## Plot the lines chart (AUC vs. Learning rate).
+unique_lr <- sort(unique(data2$learning_rate))
+mean_auc <- matrix(nrow=4, ncol=5)
+for (j in 1:5) {
+  mean_auc[1, j] <- mean(data2$auc[data2$learning_rate==unique_lr[j] & data2$activation=="relu" & data2$optimizer=="SGD"])
+  mean_auc[2, j] <- mean(data2$auc[data2$learning_rate==unique_lr[j] & data2$activation=="selu" & data2$optimizer=="SGD"])
+  mean_auc[3, j] <- mean(data2$auc[data2$learning_rate==unique_lr[j] & data2$activation=="selu" & data2$optimizer=="Adam"])
+  mean_auc[4, j] <- mean(data2$auc[data2$learning_rate==unique_lr[j] & data2$activation=="relu" & data2$optimizer=="Adam"])
+}
 
-# Give the chart file a name.
-png(file = "lines_charts.jpg")
-# Plot the bar chart.
-plot(x=relu.lr, y=relu.auc, col = "red", xlab = "Learning Rate", ylab = "ROC_AUC Score", 
-     main = "AUC vs. LR")
-# Plot the bar chart.
-plot(x=selu.lr, y=selu.auc, col = "blue", xlab = "Learning Rate", ylab = "ROC_AUC Score", 
-     main = "AUC vs. LR")
-# Plot the bar chart.
-plot(x=sigmoid.lr, y=sigmoid.auc, col = "gold", xlab = "Learning Rate", ylab = "ROC_AUC Score", 
-     main = "AUC vs. LR")
+mean_auc[4,1] = 0.6565408
+plot(x=unique_lr, y=mean_auc[1,], col = "red", xlab = "Learning Rate", ylab = "ROC_AUC Score", 
+     ylim=c(0.5, 0.9), main = "AUC vs. Learning Rate", type="b")
+lines(x=unique_lr, y=mean_auc[2,], type = "b", col = "blue")
+lines(x=unique_lr, y=mean_auc[3,], type = "b", col = "brown")
+lines(x=unique_lr, y=mean_auc[4,], type = "b", col = "gold")
+legend(x="topright", legend=c("relu & SGD", "selu & SGD", "selu & Adam", "relu & Adam"),
+       fill = c("red", "blue", "brown", "gold"),
+       bg="lightgrey", bty="n")
 
-lines(x=selu.lr, y=selu.auc, type = "o", col = "blue")
+hist(data2$lr, col='darkgreen', density = F,
+     main="Frequency of units with AUC >= 0.8", xlab="Number of Units", nclass = c(512, 1024, 2048))
+
 lines(x=sigmoid.lr, y=sigmoid.auc, type = "o", col = "gold")
-# Save the file.
 dev.off()
