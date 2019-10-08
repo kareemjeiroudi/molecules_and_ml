7bbd40f252b22a256cdf0450567ca8813819ad0d
Kareem <kareemjeiroudi@gmail.com>
2019-10-08 23:04:10 +0200

    Updated and cleaned the clutter in repository (all irrelevant files including ones from old Grid Search are removed)

--- draft/print_best_auc_score.py
+++
@@ -1,9 +0,0 @@
-max = 0.5
-fp = open("OUTPUT_02.txt")
-for line in fp.readlines():
-	if line.startswith("auc"):
-		auc_score = float(line.split(' ')[2])
-		if auc_score > max:
-			max = auc_score
-fp.close()
-print("The best found score is {}".format(max))
